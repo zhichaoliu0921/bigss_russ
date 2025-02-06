d467cd4173db4f1e050b55cc75e1268dc360c839
Zhichao <zliu157@ucr.edu>
2025-02-06 14:06:52 -0600

    bringup ur.launch

---
+++ bigss_russ_gazebo/src/FullSystem.cc
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2022 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+// We'll use a string and the gzmsg command below for a brief example.
+// Remove these includes if your plugin doesn't need them.
+#include <string>
+#include <gz/common/Console.hh>
+
+// This header is required to register plugins. It's good practice to place it
+// in the cc file, like it's done here.
+#include <gz/plugin/Register.hh>
+
+// Don't forget to include the plugin's header.
+#include "bigss_russ_gazebo/FullSystem.hh"
+
+// This is required to register the plugin. Make sure the interfaces match
+// what's in the header.
+IGNITION_ADD_PLUGIN(
+    bigss_russ_gazebo::FullSystem,
+    gz::sim::System,
+    bigss_russ_gazebo::FullSystem::ISystemConfigure,
+    bigss_russ_gazebo::FullSystem::ISystemPreUpdate,
+    bigss_russ_gazebo::FullSystem::ISystemUpdate,
+    bigss_russ_gazebo::FullSystem::ISystemPostUpdate
+)
+
+namespace bigss_russ_gazebo 
+{
+
+void FullSystem::Configure(const gz::sim::Entity &_entity,
+                const std::shared_ptr<const sdf::Element> &_element,
+                gz::sim::EntityComponentManager &_ecm,
+                gz::sim::EventManager &_eventManager)
+{
+  igndbg << "bigss_russ_gazebo::FullSystem::Configure on entity: " << _entity << std::endl;
+}
+
+void FullSystem::PreUpdate(const gz::sim::UpdateInfo &_info,
+                           gz::sim::EntityComponentManager &_ecm)
+{
+  if (!_info.paused && _info.iterations % 1000 == 0)
+  {
+    igndbg << "bigss_russ_gazebo::FullSystem::PreUpdate" << std::endl;
+  }
+}
+
+void FullSystem::Update(const gz::sim::UpdateInfo &_info,
+                        gz::sim::EntityComponentManager &_ecm)
+{
+  if (!_info.paused && _info.iterations % 1000 == 0)
+  {
+    igndbg << "bigss_russ_gazebo::FullSystem::Update" << std::endl;
+  }
+}
+
+void FullSystem::PostUpdate(const gz::sim::UpdateInfo &_info,
+                            const gz::sim::EntityComponentManager &_ecm) 
+{
+  if (!_info.paused && _info.iterations % 1000 == 0)
+  {
+    igndbg << "bigss_russ_gazebo::FullSystem::PostUpdate" << std::endl;
+  }
+}
+
+}  // namespace bigss_russ_gazebo
